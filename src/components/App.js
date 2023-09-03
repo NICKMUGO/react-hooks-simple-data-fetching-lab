175cbb273cce22913462b777cd6f013e6b1305f8
Nick Mugo <nicknjuru13@gmail.com>
2023-09-03 17:42:56 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setDogImage(data.message);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); 
+
+  return (
+    <div className="App">
+      <h1>Random Dog Image</h1>
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
